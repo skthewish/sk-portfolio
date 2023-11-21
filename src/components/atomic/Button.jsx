0d7b1cf2b552f255a12200e2dbdb231d164d33bf
sk <skisme4u@gmail.com>
2023-11-21 18:34:24 +0530

    added button shadow

--- src/components/atomic/Button.jsx
+++ src/components/atomic/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const IconButton = ({ className, onClick, children }) => {
   return (
-    <button className={`bg-background rounded-full shadow-sm grid items-center ${className}`} onClick={onClick}>
+    <button className={`bg-background rounded-full button-shadow grid items-center ${className}`} onClick={onClick}>
       {children}
     </button>
   );
